2f151ad948bf9d647599ba97359aa3ad6fe2e835
TeryKing <tery_x3@hotmail.com>
2022-09-24 11:43:34 -0500

    making initial connection and initial options

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const inquirer = require("inquirer");
-const cTable = require("console.table");
+require("console.table");
 const mysql = require("mysql");
+
 
 
 const connection = mysql.createConnection({
@@ -11,3 +12,49 @@ const connection = mysql.createConnection({
     database: "employeeDB"
 });
 
+
+
+function startprompt(){
+    inquirer
+    .prompt({
+        type: "list",
+        name:"task",
+        message: "Please select the following",
+        choices:[
+            "View Employee",
+            "Add Employee",
+            "Remove Employee",
+            "Update Roles",
+            "Add Roles",
+            "End"
+        ]
+    })
+    .then(function ({task}){
+        switch(task){
+            case "View Employee": viewEmployee();
+            break;
+
+            case "Add Employee": addEmployee();
+            break;
+
+            case "Remove Employee": removeEmployee();
+            break;
+
+            case "Update Roles": updateRoles();
+            break;
+
+            case "Add Roles": addRoles();
+            break;
+
+            case "End": connection.end();
+            break;
+        }
+    })
+}
+
+connection.connect(function (err){
+    if(err) throw err;
+    console.log("connected as id " + connection.threadId);
+    console.log("Employee Manager");
+    startprompt();
+})
