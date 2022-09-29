0641e1eaef69623aa097e466289ae501a7ef9dde
TeryKing <tery_x3@hotmail.com>
2022-09-28 19:12:56 -0500

    hiding password

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const connection = mysql.createConnection({
     host:"localhost",
     port: 3306,
     user:"root",
-    password: "Onesaint12!",
+    password: "Please Enter Password here, because I dont want to show you mine",
     database: "employeeDB"
 });
 
